9ce03a2cb9680bf5e734a45ef65125a77fbd7476
mkpdev <mukpdev11@gmail.com>
2021-08-05 16:40:02 +0530

    Add Ship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_110441) do
+ActiveRecord::Schema.define(version: 2021_08_05_110627) do
+
+  create_table "ships", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
